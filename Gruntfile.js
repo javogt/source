c4000ad12d48b86386507b0780b61a3b0e135c2f
javogt <jakobavogt@gmail.com>
2015-10-26 13:43:45 +0100

    adding postCss with autoprefixer

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,23 @@
 module.exports = function(grunt) {
- 
-   
- 
+
+	grunt.initConfig({
+
+		postcss: {
+
+			options: {
+				processors: [
+					require('autoprefixer')()
+				]
+			},
+			dist: {
+				src: 'css/style.css',
+				dest: 'main.css'
+			}
+
+		}
+
+	});
+
+	grunt.loadNpmTasks('grunt-postcss');
+
 };
